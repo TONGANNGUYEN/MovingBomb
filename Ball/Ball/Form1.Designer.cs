68f7a14b431ebd1bf4e03702d17e5686226b6eb7
Tong An Nguyen <128988861+TONGANNGUYEN@users.noreply.github.com>
2023-06-27 21:47:08 +0700

    Add files via upload

---
+++ Ball/Ball/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBall
+            // 
+            this.picBall.Image = global::Ball.Properties.Resources.bomb_icon_2;
+            this.picBall.Location = new System.Drawing.Point(172, 259);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(100, 100);
+            this.picBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBall.TabIndex = 0;
+            this.picBall.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1022, 601);
+            this.Controls.Add(this.picBall);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Ball/Ball/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ball
+{
+    public partial class Form1 : Form
+    {
+
+        int dx = 5;
+        int dy = 7;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (picBall.Left <= 0 || picBall.Right >= ClientRectangle.Width)
+                dx = -dx;
+            if (picBall.Top <= 0 || picBall.Bottom >= ClientRectangle.Height)
+                dy = -dy;
+            picBall.Left += dx;
+            picBall.Top += dy;
+        }
+    }
+}
